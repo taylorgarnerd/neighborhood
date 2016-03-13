c9b21212d59fc3bc8839c9088e1a193119752fe5
taylorgarnerd <taylorgarnerd@gmail.com>
2016-03-12 23:20:46 -0500

    ViewModel/Map separation
    Removed map from any ViewModel bindings for simplicity

--- js/app.js
+++ js/app.js
@@ -16,106 +16,31 @@ var initialLocations = [
     }
 ]
 
-var Location = function (data) {
-    this.name = ko.observable(data.name);
-    this.lat = ko.observable(data.lat);
-    this.lng = ko.observable(data.lng);
-}
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 43.2709639, lng: 11.8606963},
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    streetViewControl: false
+});
+
+initialLocations.forEach(function (loc) {
+        map.marker = new google.maps.Marker ({
+            map: map,
+            position: {lat: loc.lat, lng: loc.lng},
+            title: this.name
+        });
+    });
 
 var ViewModel = function () {
 	var self = this;
 
-    self.locations = ko.observableArray([]);
+    self.locations = initialLocations;
 
-    initialLocations.forEach(function (loc) {
-        self.locations.push(new Location(loc));
-    });
-
-    self.mapView = ko.observable(self.locations()[0]);
-
-    self.changeView = function (loc) {
-        console.log("in change view");
-        self.mapView = ko.observable(loc);
+    self.reCenter = function (loc) {
+        map.setCenter({lat: loc.lat, lng: loc.lng});
     }
 
 }
-
-ko.bindingHandlers.map = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var mapObj = ko.utils.unwrapObservable(valueAccessor());
-        //console.log(allBindings.has('name'));
-
-        var latLng = new google.maps.LatLng(
-                        ko.utils.unwrapObservable(mapObj.lat),
-                        ko.utils.unwrapObservable(mapObj.lng));
-
-        var mapOptions = { center: latLng,
-                          zoom: 7, 
-                          mapTypeId: google.maps.MapTypeId.ROADMAP,
-                          mapTypeControl: false,
-                          streetViewControl: false};
-        
-        mapObj.googleMap = new google.maps.Map(element, mapOptions);
-        
-
-        initialLocations.forEach(function (loc) {
-            mapObj.marker = new google.maps.Marker({
-                map: mapObj.googleMap,
-                position: {lat: loc.lat, lng: loc.lng},
-                title: loc.name,
-            });
-        }); 
-        
-        mapObj.onChangedCoord = function(newValue) {
-            console.log("in changed coord");
-            var latLng = new google.maps.LatLng(
-                ko.utils.unwrapObservable(mapObj.lat),
-                ko.utils.unwrapObservable(mapObj.lng));
-            mapObj.googleMap.setCenter(latLng);                 
-        };
-        
-        /*mapObj.onMarkerMoved = function(dragEnd) {
-            var latLng = mapObj.marker.getPosition();
-            mapObj.lat(latLng.lat());
-            mapObj.lng(latLng.lng());
-        };*/
-        
-        //mapObj.subscribe(mapObj.onChangedCoord);
-        //mapObj.lng.subscribe(mapObj.onChangedCoord);
-
-        viewModel.mapView.subscribe(mapObj.onChangedCoord);
-
-        //mapObj.loc.subscribe(mapObj.onChangedCoord);
-        
-        //google.maps.event.addListener(mapObj.marker, 'dragend', mapObj.onMarkerMoved);
-        
-        //$("#" + element.getAttribute("id")).data("mapObj",mapObj);
-    },
-
-    /*update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var mapObj = ko.utils.unwrapObservable(valueAccessor());
-
-        var latLng = new google.maps.LatLng(
-                        ko.utils.unwrapObservable(mapObj.lat),
-                        ko.utils.unwrapObservable(mapObj.lng));
-
-        var mapOptions = { center: latLng,
-                          zoom: 7, 
-                          mapTypeId: google.maps.MapTypeId.ROADMAP,
-                          mapTypeControl: false,
-                          streetViewControl: false};
-        
-        mapObj.googleMap = new google.maps.Map(element, mapOptions);
-        
-
-        initialLocations.forEach(function (loc) {
-            mapObj.marker = new google.maps.Marker({
-                map: mapObj.googleMap,
-                position: {lat: loc.lat, lng: loc.lng},
-                title: loc.name,
-            });
-        }); 
-    }*/
-};
 
 ko.applyBindings(new ViewModel());
